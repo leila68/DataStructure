d70a32e9f8546cc46387d13d0003f29af03599f6
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-05 23:09:00 +0330

    updated queue.cs

---
+++ stackQueue/queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stackQueue
+{
+    class queue:stack
+    {
+        private object[] queueArray = new object[5];
+        private int front;
+        private int rear;
+        
+        
+        public queue()
+        {
+            front = 0;
+            rear = 0;
+        }
+        public void printQ()
+        {
+            for (int i = 0; i < queueArray.Length; i++)
+            {
+                Console.WriteLine(queueArray[i]);
+            }
+        }
+        public  bool emptyCheck()
+        {
+            if (rear == front)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void insertQue(object a)
+        {
+           
+            if (rear == queueArray.Length)
+            {
+                Console.WriteLine("queue is full!");
+            }
+            else
+            {
+                queueArray[rear] = a;
+
+                rear++;
+            }
+                 
+        }
+
+        public void delQue()
+        {
+            if (front == rear)
+            {
+                Console.WriteLine("queue is empty!");
+            }
+            else 
+            {
+                queueArray[front] = null;
+                front++;
+            }
+         
+        }
+        
+    }
+
+   
+}
